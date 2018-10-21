3c1994ad0d2a3c915a157f0957611b632fde16f9
t03jam8 <grovejam@gmail.com>
2018-10-21 13:53:01 +0800

    Raw data files for export

---
+++ data/index.R
@@ -0,0 +1,6 @@
+
+# importing data 
+plantedCarbon <- raster("./data/PlantedAreaCarbon.tif")
+unPlantedCarbon <- raster("./data/UnPlantedAreaCarbon.tif")
+fullAreaCarbon <- raster("./data/FullAreaCarbon.tif")
+coupeYear <- raster("./data/CoupeYear.tif")
