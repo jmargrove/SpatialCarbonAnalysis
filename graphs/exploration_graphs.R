33690b806a96244c817767c2a2be73fcae66accf
t03jam8 <grovejam@gmail.com>
2018-10-21 13:53:52 +0800

    Viewing the graphs

---
+++ graphs/exploration_graphs.R
@@ -0,0 +1,11 @@
+# @title inla spde analaysis 
+
+# importing packages
+source("./packages.R")
+# importing data 
+source("./data/organised_data.R")
+
+ggplot(data, aes(x = x, y = y, fill = carbon)) + geom_raster() + theme_bw()
+ggplot(data, aes(x = x, y = y, fill = treatment)) + geom_raster() + theme_bw()
+ggplot(data, aes(x = x, y = y, fill = coupeYear)) + geom_raster() + theme_bw()
+
