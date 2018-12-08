f37aadeb5bf2417e4302f6a805d1383e6ca4296b
t03jam8 <grovejam@gmail.com>
2018-12-08 18:33:40 +0100

    Removed data files

--- data/index.R
+++
@@ -1,5 +0,0 @@
-# importing data 
-plantedCarbon <- raster("./data/PlantedAreaCarbon.tif")
-unPlantedCarbon <- raster("./data/UnPlantedAreaCarbon.tif")
-fullAreaCarbon <- raster("./data/FullAreaCarbon.tif")
-coupeYear <- raster("./data/CoupeYear.tif")
